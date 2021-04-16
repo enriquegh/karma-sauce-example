74e5249e941fe779052c9223f637c23df756219c
Enrique Gonzalez <enrique@saucelabs.com>
2021-04-16 10:04:43 -0700

    initial commit

---
+++ karma.conf-ci.js
@@ -0,0 +1,64 @@
+module.exports = function (config) {
+  if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+    console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+    process.exit(1)
+  }
+
+  // Browsers to run on Sauce Labs
+  // Check out https://saucelabs.com/platforms for all browser/OS combos
+  const customLaunchers = {
+    // sl_chrome: {
+    //   base: 'SauceLabs',
+    //   browserName: 'chrome',
+    //   version: 'latest',
+    //   tags: ['jsonwp-chrome']
+    // },
+    // sl_chromeW3C: {
+    //   base: 'SauceLabs',
+    //   browserName: 'chrome',
+    //   browserVersion: 'latest',
+    //   'sauce:options':{
+    //     tags: ['w3c-chrome']
+    //   }
+    // },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: 'latest',
+      tags: ['jsonwp-firefox']
+    },
+    // sl_ie_11: {
+    //   base: 'SauceLabs',
+    //   browserName: 'internet explorer',
+    //   version: 'latest',
+    //   tags: ['jsonwp-ie11']
+    // }
+  };
+
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'src/*.js',
+      'test/*.js'
+    ],
+    logLevel: config.LOG_DEBUG,
+    reporters: ['progress', 'saucelabs'],
+    port: 9876,
+    colors: true,
+    sauceLabs: {
+      testName: 'Karma and Sauce Labs demo',
+      recordScreenshots: false,
+      startConnect: true,
+      connectOptions: {
+        logfile: 'sauce_connect.log'
+      },
+      public: 'public'
+    },
+    // Increase timeout in case connection in CI is slow
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    singleRun: true
+  })
+};
